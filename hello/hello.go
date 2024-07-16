86124018874d06c07c28942e31ffc9173c93c880
ruthrootz <rutholdja@icloud.com>
2024-07-16 18:12:16 -0400

    refactor

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,10 @@ import (
   "fmt"
 )
 
+const englishGreetingPrefix = "Hello, "
+
 func Hello(name string) string {
-  return fmt.Sprintf("Hello, %s!", name)
+  return fmt.Sprintf("%s%s!", englishGreetingPrefix, name)
 }
 
 func main() {
