3dd825b43886765d0b7deb92cebaca914e06a8f8
Ruth Oldja <rutholdja@icloud.com>
2024-08-16 10:58:47 -0400

    added Spanish and French

--- hello/hello.go
+++ hello/hello.go
@@ -4,16 +4,27 @@ import (
   "fmt"
 )
 
-const englishGreetingPrefix = "Hello, "
+const spanish = "Spanish"
+const english = "English"
+const french = "French"
 
-func Hello(name string) string {
+const englishGreetingPrefix = "Hello, "
+const spanishGreetingPrefix = "Hola, "
+const frenchGreetingPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
   if name == "" {
     name = "World"
+  }
+  if language == spanish {
+    return fmt.Sprintf("%s%s!", spanishGreetingPrefix, name)
+  } else if language == french {
+    return fmt.Sprintf("%s%s!", frenchGreetingPrefix, name)
   }
   return fmt.Sprintf("%s%s!", englishGreetingPrefix, name)
 }
 
 func main() {
-  fmt.Println(Hello("Ruth"))
+  fmt.Println(Hello("Ruth", ""))
 }
 
