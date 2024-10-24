b8318cf5d403622b3dffc7d5101adc8c85f5a2fc
Ruth Oldja <rutholdja@icloud.com>
2024-10-24 17:49:00 -0400

    use Shape interface

--- structsMethodsInterfaces/shapes_test.go
+++ structsMethodsInterfaces/shapes_test.go
@@ -16,24 +16,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+  checkArea := func(t testing.TB, shape Shape, want float64) {
+    t.Helper()
+    got := shape.Area()
+    if got != want {
+      t.Errorf("got %g, want %g", got, want)
+    }
+  }
+
   t.Run("rectangles", func(t *testing.T) {
     r := Rectangle {12, 6}
-    got := r.Area()
-    want := 72.0
-
-    if got != want {
-      t.Errorf("got %g want %g", got, want)
-    }
+    checkArea(t, r, 72.0)
   })
 
   t.Run("circles", func(t *testing.T) {
     c := Circle {10}
-    got := c.Area()
-    want := 314.1592653589793
-
-    if got != want {
-      t.Errorf("got %g want %g", got, want)
-    }
+    checkArea(t, c, 314.1592653589793)
   })
 
 }
