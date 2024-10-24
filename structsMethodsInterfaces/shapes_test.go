3c870c60f81e024876ec136257b2ac7b5a92811f
Ruth Oldja <rutholdja@icloud.com>
2024-10-24 17:30:11 -0400

    Perimeter()

---
+++ structsMethodsInterfaces/shapes_test.go
@@ -0,0 +1,15 @@
+package structsMethodsInterfaces
+
+import (
+  "testing"
+)
+
+func TestPerimiter(t *testing.T) {
+  got := Perimeter(10.0, 10.0)
+  want := 40.0
+
+  if got != want {
+    t.Errorf("got %.2f, want %.2f", got, want)
+  }
+}
+
