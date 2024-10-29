5ba6da50f67d2023b223667d12ffcfb27acc36a4
Ruth Oldja <rutholdja@icloud.com>
2024-10-29 17:12:03 -0400

    tests not working...

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,18 +5,28 @@ import (
 )
 
 var ErrorNotFound = errors.New("could not find the word you were looking for")
+var ErrorWordExists = errors.New("word already exists")
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(searchTerm string) (string, error) {
-  result, ok := d[searchTerm]
+func (d Dictionary) Search(word string) (string, error) {
+  result, ok := d[word]
   if !ok {
     return "", ErrorNotFound
   }
   return result, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-  d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+  _, err := d.Search(word)
+  switch err {
+  case ErrorNotFound:
+    d[word] = definition
+  case nil:
+    return ErrorWordExists
+  default:
+    return err
+  }
+  return nil
 }
 
