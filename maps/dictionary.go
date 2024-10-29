070467a389abe01b440411788c89b2177bbeb4ac
Ruth Oldja <rutholdja@icloud.com>
2024-10-29 16:10:38 -0400

    use ok

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,10 +7,10 @@ import (
 type Dictionary map[string]string
 
 func (d Dictionary) Search(searchTerm string) (string, error) {
-  result := d[searchTerm]
-  if result == "" {
+  result, ok := d[searchTerm]
+  if !ok {
     return "", errors.New("could not find the word you were looking for")
   }
-  return d[searchTerm], nil
+  return result, nil
 }
 
