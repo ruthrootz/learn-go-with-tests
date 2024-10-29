2719a25fecdbebecc9a41bb9139b5c803f4a9976
Ruth Oldja <rutholdja@icloud.com>
2024-10-29 16:14:24 -0400

    refactor to use error

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,12 +4,14 @@ import (
   "errors"
 )
 
+var ErrorNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(searchTerm string) (string, error) {
   result, ok := d[searchTerm]
   if !ok {
-    return "", errors.New("could not find the word you were looking for")
+    return "", ErrorNotFound
   }
   return result, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -15,17 +15,23 @@ func TestSearch(t *testing.T) {
 
   t.Run("unknown word", func(t *testing.T) {
     _, err := dictionary.Search("unknown")
-    want := "could not find the word you were looking for"
     if err == nil {
       t.Fatal("expected to get an error.")
     }
-    assertStrings(t, err.Error(), want)
+    assertError(t, err, ErrorNotFound)
   })
 }
 
 func assertStrings(t testing.TB, got, want string) {
   t.Helper()
   if got != want {
+    t.Errorf("got %q want %q given %q", got, want, "test")
+  }
+}
+
+func assertError(t testing.TB, got, want error) {
+  t.Helper()
+  if got.Error() != want.Error() {
     t.Errorf("got %q want %q given %q", got, want, "test")
   }
 }
