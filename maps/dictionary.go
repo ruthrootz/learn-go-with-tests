ee439383fe5929d315ca037260bf2e214459074a
Ruth Oldja <rutholdja@icloud.com>
2024-10-29 16:09:40 -0400

    return error if word not found

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,8 +1,16 @@
 package maps
 
+import (
+  "errors"
+)
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(searchTerm string) string {
-  return d[searchTerm]
+func (d Dictionary) Search(searchTerm string) (string, error) {
+  result := d[searchTerm]
+  if result == "" {
+    return "", errors.New("could not find the word you were looking for")
+  }
+  return d[searchTerm], nil
 }
 
