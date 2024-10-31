fd3bb0bcb8d7ee43482230083f4d530188b9c267
ruthrootz <rutholdja@icloud.com>
2024-10-31 19:06:20 -0400

    handler

--- dependencyInjection/di.go
+++ dependencyInjection/di.go
@@ -3,9 +3,14 @@ package dependencyInjection
 import (
   "fmt"
   "io"
+  "net/http"
 )
 
 func Greet(writer io.Writer, name string) {
   fmt.Fprintf(writer, "Hello, %s!", name)
+}
+
+func GreeterHandler(w http.ResponseWriter, r *http.Request) {
+  Greet(w, "world")
 }
 
