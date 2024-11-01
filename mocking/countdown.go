35ad77be3cea8b25fc47792e73cd51d1f477b875
ruthrootz <rutholdja@icloud.com>
2024-10-31 20:32:12 -0400

    time.sleep

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package mocking
 import (
   "io"
   "fmt"
+  "time"
 )
 
 var countdownStart = 3
@@ -11,6 +12,7 @@ var finalWord = "Go!"
 func Countdown(out io.Writer) {
   for i := countdownStart; i > 0; i-- {
     fmt.Fprintln(out, i)
+    time.Sleep(1 * time.Second)
   }
   fmt.Fprintf(out, finalWord)
 }
