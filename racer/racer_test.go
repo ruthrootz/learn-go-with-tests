753ec8372baa4615e95d4e74dc77dbdb62cb1d0d
Ruth Oldja <rutholdja@icloud.com>
2024-11-27 16:34:53 -0500

    test passes every time

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -2,17 +2,33 @@ package racer
 
 import (
   "testing"
+  "time"
+  "net/http"
+  "net/http/httptest"
 )
 
 func TestRacer(t *testing.T) {
-  slowUrl := "http://www.facebook.com"
-  fastUrl := "http://www.quii.dev"
+  slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    time.Sleep(20 * time.Millisecond)
+    w.WriteHeader(http.StatusOK)
+  }))
+  fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+  }))
 
-  want := fastUrl
-  got := Racer(slowUrl, fastUrl)
+  //slowUrl := "http://www.facebook.com"
+  //fastUrl := "http://www.quii.dev"
+  slowURL := slowServer.URL
+  fastURL := fastServer.URL
+
+  want := fastURL
+  got := Racer(slowURL, fastURL)
 
   if got != want {
     t.Errorf("got %q want %q", got, want)
   }
+
+  slowServer.Close()
+  fastServer.Close()
 }
 
