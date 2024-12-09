463c5aa60418e8649a37ef107594fa772cc86bd7
Ruth Oldja <rutholdja@icloud.com>
2024-12-09 16:29:30 -0500

    switch case

--- reflection/walk.go
+++ reflection/walk.go
@@ -10,10 +10,11 @@ func walk(x interface{}, fn func(string)) {
   for i := 0; i < val.NumField(); i++ {
     field := val.Field(i)
 
-    if field.Kind() == reflect.String {
+    switch field.Kind() {
+    case reflect.String:
       fn(field.String())
-    } else if field.Kind() == reflect.Struct {
-      walk(field, fn)
+    case reflect.Struct:
+      walk(field.Interface(), fn)
     }
   }
 }
