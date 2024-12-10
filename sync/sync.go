87640a8651d006f3a4b32e9c6f38cb7fd6840829
Ruth Oldja <rutholdja@icloud.com>
2024-12-10 14:37:49 -0500

    first test working

--- sync/sync.go
+++ sync/sync.go
@@ -1,4 +1,14 @@
 package sync
 
+type Counter struct {
+  value int
+}
 
+func (c *Counter) Inc() {
+  c.value += 1
+}
+
+func (c *Counter) Value() int {
+  return c.value
+}
 
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -4,7 +4,16 @@ import (
   "testing"
 )
 
-func TestSync(t *testing.T) {
+func TestCounter(t *testing.T) {
+  t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+    counter := Counter{}
+    counter.Inc()
+    counter.Inc()
+    counter.Inc()
 
+    if counter.Value() != 3 {
+      t.Errorf("got %d want %d", counter.Value(), 3)
+    }
+  })
 }
 
