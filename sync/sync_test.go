a430de29eafb58acbf5282a1d449b3de555b6116
Ruth Oldja <rutholdja@icloud.com>
2024-12-10 15:11:19 -0500

    add test for concurrent

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -2,9 +2,11 @@ package sync
 
 import (
   "testing"
+  "sync"
 )
 
 func TestCounter(t *testing.T) {
+
   t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
     counter := Counter{}
     counter.Inc()
@@ -13,12 +15,30 @@ func TestCounter(t *testing.T) {
 
     assertCounter(t, counter, 3)
   })
+
+  t.Run("runs safely concurrently", func(t *testing.T) {
+    wantedCount := 1000
+    counter := Counter{}
+
+    var wg sync.WaitGroup
+    wg.Add(wantedCount)
+
+    for i := 0; i < wantedCount; i++ {
+      go func() {
+        counter.Inc()
+        wg.Done()
+      }()
+    }
+    wg.Wait()
+
+    assertCounter(t, counter, wantedCount)
+  })
 }
 
 func assertCounter(t testing.TB, got Counter, want int) {
   t.Helper()
   if got.Value() != want {
-    t.Errorf("got %d want %d", got.Value(), 3)
+    t.Errorf("got %d want %d", got.Value(), want)
   }
 }
 
