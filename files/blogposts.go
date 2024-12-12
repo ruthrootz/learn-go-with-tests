b5dacbcc680bc5283d6254f27838e4b428f14d75
Ruth Oldja <rutholdja@icloud.com>
2024-12-12 16:19:06 -0500

    refactor

--- files/blogposts.go
+++ files/blogposts.go
@@ -1,8 +1,12 @@
 package blogposts
 
 import (
+  "bufio"
+  "bytes"
+  "fmt"
   "io"
   "io/fs"
+  "strings"
 )
 
 type Post struct {
@@ -37,13 +41,38 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
   return newPost(postFile)
 }
 
-func newPost(postFile fs.File) (Post, error) {
-  postData, err := io.ReadAll(postFile)
-  if err != nil {
-    return Post{}, err
-  }
+const (
+  titleSeparator        = "Title: "
+  descriptionSeparator  = "Description: "
+  bodySeparator         = "Body: "
+  tagsSeparator         = "Tags: "
+)
 
-  post := Post{Title: string(postData)[7:]}
-  return post, nil
+func newPost(postBody io.Reader) (Post, error) {
+  scanner := bufio.NewScanner(postBody)
+
+  readMetaLine := func(tagName string) string {
+    scanner.Scan()
+    return strings.TrimPrefix(scanner.Text(), tagName)
+}
+
+  title := readMetaLine(titleSeparator)
+  description := readMetaLine(descriptionSeparator)
+  tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+
+  scanner.Scan() // ignore a line
+
+  buf := bytes.Buffer{}
+  for scanner.Scan() {
+    fmt.Fprintln(&buf, scanner.Text())
+  }
+  body := strings.TrimSuffix(buf.String(), "\n")
+
+  return Post{
+    Title:       title,
+    Description: description,
+    Tags:        tags,
+    Body:        body,
+  }, nil
 }
 
