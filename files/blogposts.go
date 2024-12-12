2db5570a4f293ca79b6c6a8900f2e8e3f4b54b2e
Ruth Oldja <rutholdja@icloud.com>
2024-12-12 15:50:54 -0500

    passing

---
+++ files/blogposts.go
@@ -0,0 +1,20 @@
+package blogposts
+
+import (
+  "io/fs"
+)
+
+type Post struct {
+  Title, Description, Body string
+  Tags []string
+}
+
+func NewPostsFromFS(fileSystem fs.FS) []Post {
+  dir, _ := fs.ReadDir(fileSystem, ".")
+  var posts []Post
+  for range dir {
+    posts = append(posts, Post{})
+  }
+  return posts
+}
+
