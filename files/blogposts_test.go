01f7388d3752aab451c24f250102727be410e116
Ruth Oldja <rutholdja@icloud.com>
2024-12-12 16:02:27 -0500

    get title

--- files/blogposts_test.go
+++ files/blogposts_test.go
@@ -1,14 +1,15 @@
 package blogposts
 
 import (
+  "reflect"
   "testing"
   "testing/fstest"
 )
 
 func TestNewBlogPosts(t *testing.T) {
   fs := fstest.MapFS{
-    "hello world.md":  {Data: []byte("hi")},
-    "hello-world2.md": {Data: []byte("hola")},
+    "hello world.md":  {Data: []byte("Title: Post 1")},
+    "hello-world2.md": {Data: []byte("Title: Post 2")},
   }
 
   posts, err := NewPostsFromFS(fs)
@@ -19,6 +20,13 @@ func TestNewBlogPosts(t *testing.T) {
 
   if len(posts) != len(fs) {
     t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+  }
+
+  got := posts[0]
+  want := Post{Title: "Post 1"}
+
+  if !reflect.DeepEqual(got, want) {
+    t.Errorf("got %+v, want %+v", got, want)
   }
 }
 
